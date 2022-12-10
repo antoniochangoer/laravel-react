62cef1047c979dc9e8294685b879ef84ef02a92b
nishal89 <nishal_adhin@hotmail.com>
2022-12-10 23:12:30 +0100

    fix chirp

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -3,12 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\ChirpCreated;
-use App\Models\User;
-use App\Notifications\NewChirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendChirpCreatedNotifications implements ShouldQueue
+class SendChirpCreatedNotifications
 {
     /**
      * Create the event listener.
@@ -28,8 +26,6 @@ class SendChirpCreatedNotifications implements ShouldQueue
      */
     public function handle(ChirpCreated $event)
     {
-        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
-            $user->notify(new NewChirp($event->chirp));
-        }
+
     }
 }
